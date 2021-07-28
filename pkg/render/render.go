33d1296c4314f0988527151785e89e7c1a39c37b
Paulo Vidal <58638884+u1d4lp@users.noreply.github.com>
2021-07-28 12:21:05 -0300

    Update render.go

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,8 +1,10 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -11,18 +13,28 @@ var functions = template.FuncMap{}
 
 // RenderTemplate renders templates using html/templates
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	_, err := RenderTemplateTest(w)
+	tc, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("Error getting template cache:", err)
+		log.Fatal(err)
 	}
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err = parsedTemplate.Execute(w, nil)
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	_ = t.Execute(buf, nil)
+
+	_, err = buf.WriteTo(w)
 	if err != nil {
-		fmt.Println("Error parsing template", err)
+		fmt.Println("Error writing template to browser", err)
 	}
 }
 
-func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, error) {
+// CreateTemplateCache creates a template cache as a map
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
@@ -30,8 +42,6 @@ func RenderTemplateTest(w http.ResponseWriter) (map[string]*template.Template, e
 	}
 	for _, page := range pages {
 		name := filepath.Base(page)
-
-		fmt.Println("Page is currently", page)
 
 		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
